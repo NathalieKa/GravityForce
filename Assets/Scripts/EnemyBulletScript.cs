7f1c0b8bb95934432a413727e6f4030145fce8f5
NathalieKa <119058071+NathalieKa@users.noreply.github.com>
2024-12-22 23:10:25 +0100

    Fehler korrigiert

--- Assets/Scripts/EnemyBulletScript.cs
+++ Assets/Scripts/EnemyBulletScript.cs
@@ -8,6 +8,8 @@ public class EnemyBulletScript : MonoBehaviour
     private Rigidbody2D rb;
     public float force;
 
+    private float timer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,6 +28,21 @@ public class EnemyBulletScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+
+        if (timer > 10)
+        {
+            Destroy(gameObject);
+        }
         
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Rocket"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
 }
